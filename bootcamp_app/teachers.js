88b2e69c751366494da94ca00fd1a32316c609c7
Lea Pehar <leapehar@gmail.com>
2022-02-11 02:23:06 +0000

    updated students.js and teachers.js to use parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -72,7 +72,7 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $2
 ORDER BY teacher;
 `)
   .then(res => {
